4e8e2a29f795d09cabe731d862b2714fb35403d3
React330 <73373287+React330@users.noreply.github.com>
2022-04-15 08:10:49 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 import "./styles.css";
+
+import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値(inputのid名)を取得し、入力した文を初期化(空文字)にする
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 押された完了ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // divタグの子要素に各要素を配置する
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストを追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+// 未完了リスト指定の要素を削除
+const deleteFromIncompleteList = () => {};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
