cf13e019afa761b4d43a979005626318133904c9
React330 <73373287+React330@users.noreply.github.com>
2022-04-16 10:51:43 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import "./styles.css";
 
-import "./styles.css";
-
 const onClickAdd = () => {
   // テキストボックスの値(inputのid名)を取得し、入力した文を初期化(空文字)にする
   const inputText = document.getElementById("add-text").value;
@@ -20,8 +18,31 @@ const onClickAdd = () => {
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
     // 押された完了ボタンの親タグ(div)を未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストの取得
+    const text = addTarget.firstElementChild.innerHTML;
+
+    // div以下を初期化(削除)
+    addTarget.textContent = null;
+
+    // liタグの生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // buttonタグの生成
+    const buckButton = document.createElement("button");
+    buckButton.innerText = "戻す";
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(buckButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)タグ生成
@@ -29,8 +50,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     // 押された削除ボタンの親タグ(div)を未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を配置する
@@ -43,7 +63,9 @@ const onClickAdd = () => {
 };
 
 // 未完了リスト指定の要素を削除
-const deleteFromIncompleteList = () => {};
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
 
 document
   .getElementById("add-button")
